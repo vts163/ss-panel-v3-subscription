6fe15092af934dd9035781dfba8dda0ca0611e46
qinghuas <30293805+qinghuas@users.noreply.github.com>
2017-07-23 12:35:25 +0800

    修复windows

--- api.php
+++ api.php
@@ -1,19 +1,31 @@
 <?php
-//ss panel v3 登录地址
+//URL安全的Base64编码
+function urlsafe_b64encode($string) {
+   $data = base64_encode($string);
+   $data = str_replace(array('+','/','='),array('-','_',''),$data);
+   return $data;
+}
+
+//登录参数
 $ss_panel_login_address = "";
-//数据库
-$db_host = 'localhost';//地址
-$db_user = 'root';//用户名
-$db_pw = 'root';//密码
-$db_name = 'sspanel';//数据库表
-//Group
+$username = htmlspecialchars($_GET['user']);//用户名
+$password = htmlspecialchars($_GET['passwd']);//密码
+$two_step_verification_code = htmlspecialchars($_GET['code']);//两步验证码
+//数据库参数
+$db_host = 'localhost';$db_user = 'root';$db_pw = 'root';$db_name = 'sspanel';
+//Group名称
 $group_name = '';//Group名称
-$group_name_base64 = (base64_encode($group_name));//base64加密后的Group名称
+$group_name_base64 = urlsafe_b64encode($group_name);//安全base64加密后的Group名称
+//生成连接前缀参数，请勿修改
+$after_obfs = '/?obfsparam=';
+$after_server_name = '&remarks=';
+$after_group = '&group=';
+$after_ssr_url = 'ssr://';
+
+//服务器ID设置,服务器ID可在 "管理面板-节点列表" 中找到
+//$server_jp = '3';
 
 //验证账号密码正误
-$username = htmlspecialchars($_GET['user']);//接收用户名
-$password = htmlspecialchars($_GET['passwd']);//接收的密码
-$two_step_verification_code = htmlspecialchars($_GET['code']);//接收两步验证码
 $post_data = array ("email" => "$username","passwd" => "$password","code" => "$two_step_verification_code");
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $ss_panel_login_address);
@@ -28,70 +40,50 @@ $login_code = $login_results_json->{'ret'};
 $login_msg = $login_results_json->{'msg'};
 
 switch($login_code){
+//当账号密码正确时,$login_code=1
 case '1':
-//账号密码正确时
-$con = mysqli_connect($db_host, $db_user, $db_pw, $db_name) or die("Unable to connect to the database.".mysqli_error());
-//获取连接配置信息
+//执行生成函数
+function get_ssr_url($server_id,$username,$db_host,$db_user,$db_pw,$db_name,$group_name,$group_name_base64,$after_obfs,$after_server_name,$after_group,$after_ssr_url) {
+//获取连接配置
+$con = mysqli_connect($db_host, $db_user, $db_pw, $db_name) or die("Connection error.".mysqli_error());
 $sql = 'SELECT * FROM `user` WHERE `email` = \'' . $username . '\'';
 $user_config = mysqli_fetch_array(mysqli_query($con, $sql));
-
 $user_passwd = $user_config['passwd'];//密码
-$user_passwd_base64 = (base64_encode($user_passwd));//base64加密后的ssr连接密码
+$user_passwd_base64 = urlsafe_b64encode($user_passwd);//安全base64加密后的ssr连接密码
 $user_port = $user_config['port'];//端口
 $user_protocol = $user_config['protocol'];//协议
 $user_method = $user_config['method'];//加密
 $user_obfs = $user_config['obfs'];//混淆
 $user_obfs_parameter = $user_config['obfs_param'];//混淆参数
-$user_obfs_parameter_base64 = (base64_encode($user_obfs_parameter));//base64加密后的混淆参数
+$user_obfs_parameter_safe_base64 = urlsafe_b64encode($user_obfs_parameter);//安全base64加密后的混淆参数
 
-//生成连接前缀参数，请勿修改
-$after_obfs = '/?obfsparam=';
-$after_server_name = '&remarks=';
-$after_group = '&group=';
-$after_ssr_url = 'ssr://';
+//获取服务器配置 
+$sql = 'SELECT * FROM `ss_node` WHERE `id` = \'' . $server_id . '\'';
+$server_config = mysqli_fetch_array(mysqli_query($con, $sql));
+$server_address = $server_config['server'];//地址
+$server_name = $server_config['name'];//名称
+$server_name_safe_base64 = urlsafe_b64encode($server_name);//安全base64后的服务器名称
 
-/*如何添加一台提供订阅服务的服务器？
-1.为该服务器命名,然后在下方服务器列表添加,并填写服务器ID,服务器ID可在 "管理面板-节点列表" 中找到
-推荐服务器命名规则:server_地区简拼,例:
-$server_hk = '3';
-2.将下面的"Your_server_name"替换为你为服务器的命名
-//获取节点信息并生成链接
-$sql = 'SELECT * FROM `ss_node` WHERE `id` = \'' . $server_jp . '\'';
-$server_Your_server_name_config = mysqli_fetch_array(mysqli_query($con, $sql));
-$server_Your_server_name_address = $server_Your_server_name_config['server'];//地址
-$server_Your_server_name_name = $server_Your_server_name_config['name'];//名称
-$server_Your_server_name_name_base64 = (base64_encode($server_Your_server_name_name));//base64后的名称
-$array = array("$server_Your_server_name_address",":","$user_port",":","$user_protocol",":","$user_method",":","$user_obfs",":","$user_passwd_base64","$after_obfs","$user_obfs_parameter_base64","$after_server_name","$server_Your_server_name_name_base64","$after_group","$group_name_base64");
-$server_Your_server_name_url_1 = implode($array);//此处得到的是连接配置明文
-$server_Your_server_name_url_2 = (base64_encode($server_Your_server_name_url_1));//此处得到的是被base64加密的连接配置明文
-$array = array("$after_ssr_url","$server_Your_server_name_url_2");
-$server_Your_server_name_url_3 = implode($array);//此处得到完整的ssr://链接
-3.将替换后的内容粘贴到下方的"Here"
-4.参考"生成最终配置"后的注释,添加需要生成的服务器,注意修改"Your_server_name"
-5.大功告成!
-注：如需添加多台服务器,重复上述步骤即可,注意都需修改"Your_server_name"为你为服务器的命名
-不能重复,建议按照一定规律命名,以防混淆
-*/
+//生成
+$array = array("$server_address",":","$user_port",":","$user_protocol",":","$user_method",":","$user_obfs",":","$user_passwd_base64","$after_obfs","$user_obfs_parameter_safe_base64","$after_server_name","$server_name_safe_base64","$after_group","$group_name_base64");
+$server_url_1 = implode($array);//此处得到的是连接配置明文
+$server_url_2 = (base64_encode($server_url_1));//此处得到的是被base64加密的连接配置明文
+$array = array("$after_ssr_url","$server_url_2");//将ssr://与被base64加密的连接配置明文拼接
+$server_url_3 = implode($array);//此处得到完整的ssr://链接
+return $server_url_3;
+}
 
-//服务器列表
-//$server_hk = '3';
+//生成各服务器连接链接
+$server_jp_url = get_ssr_url("$server_jp","$username","$db_host","$db_user","$db_pw","$db_name","$group_name","$group_name_base64","$after_obfs","$after_server_name","$after_group","$after_ssr_url");
 
-//Here :)
-
-
-//生成最终配置
-$array = array("$server_jp_url_3","\r\n","$server_hk_url_3","\r\n","$server_sg_url_3");
-/*仅一台服务器配置应如下:
-$array = array("$server_Your_server_name_url_3");
-多台服务器配置应如下：
-$array = array("$server_Your_server_name_url_3","\r\n","$server_Your_server_name_url_3","\r\n","$server_Your_server_name_url_3");
-注:【"\r\n"】是为ssr://链接换行,只需在倒数第二个服务器后添加
-*/
+//拼接各服务器连接链接,生成最终配置,输出
+$array = array("$server_jp_url");
 $server_all_url_1 = implode($array);//此处得到的是所有完整的ssr://链接
 $server_all_url_2 = (base64_encode($server_all_url_1));//此处得到的是被base64加密后的所有完整的ssr://链接
 echo "$server_all_url_2";
 break;
-//账号密码错误时
+
+//当账号密码错误时,$login_code=0
 case '0':
 echo "登录失败，用户名或密码不正确。";
 break;
